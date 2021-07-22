440cd5f0ad265703ac45856e4fcd085e8d15a49e
Stone Pack <stone.pack@outlook.com>
2021-07-21 21:35:55 -0700

    added actions after sending login request

--- lib/bloc/login.bloc.dart
+++ lib/bloc/login.bloc.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
-
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:login/routes/success.dart';
 
 class LoginController extends GetxController {
   RxString token = ''.obs;
@@ -10,7 +11,7 @@ class LoginController extends GetxController {
   TextEditingController usernameController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
 
-  login() async {
+  login(BuildContext context) async {
     Uri url = Uri.parse('http://localhost:4242/api/login');
 
     var response = await http.post(
@@ -27,12 +28,23 @@ class LoginController extends GetxController {
     Map<String, dynamic> parsedResponse = jsonDecode(response.body);
 
     if (response.statusCode == 200) {
-      token.value = parsedResponse['token'];
-      // TODO: Navigate to a new page that displays the token
-      // Get.to(() {});
+      token.value = '${parsedResponse['token']}';
+      Get.to(() => SuccessPage());
     } else {
-      // TODO: Add toast notification with error
-      print('${parsedResponse['error']}');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.black45,
+          behavior: SnackBarBehavior.floating,
+          content: Text(
+            '${parsedResponse['error']}',
+            style: TextStyle(
+              color: Colors.red,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          margin: EdgeInsets.all(20),
+        ),
+      );
     }
   }
 }
