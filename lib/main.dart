a3c6a37a47973607e2a6e08c0fc6e974aacb5342
Stone Pack <stone.pack@outlook.com>
2021-07-21 20:52:32 -0700

    added get library for bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login/bloc/login.bloc.dart';
 import 'package:login/routes/login.dart';
 
 void main() {
@@ -6,9 +8,11 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  final LoginController c = Get.put(LoginController());
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Login Demo',
       home: Scaffold(
         body: LoginPage(),
