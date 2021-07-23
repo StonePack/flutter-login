72babb15bd74235ee2c585752fc1a52ae27bd03d
Stone Pack <stone.pack@outlook.com>
2021-07-22 21:21:46 -0700

    added snachbar for connection error instead of app crashing

--- lib/bloc/login.bloc.dart
+++ lib/bloc/login.bloc.dart
@@ -12,39 +12,49 @@ class LoginController extends GetxController {
   TextEditingController passwordController = new TextEditingController();
 
   login(BuildContext context) async {
-    Uri url = Uri.parse('http://localhost:4242/api/login');
+    Uri url = Uri.parse('http://localhost:4747/login');
 
-    var response = await http.post(
-      url,
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'user': '${usernameController.text}',
-        'password': '${passwordController.text}',
-      }),
-    );
-
-    Map<String, dynamic> parsedResponse = jsonDecode(response.body);
-
-    if (response.statusCode == 200) {
-      token.value = '${parsedResponse['token']}';
-      Get.to(() => SuccessPage());
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.black45,
-          behavior: SnackBarBehavior.floating,
-          content: Text(
-            '${parsedResponse['error']}',
-            style: TextStyle(
-              color: Colors.red,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          margin: EdgeInsets.all(20),
-        ),
+    try {
+      var response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'user': '${usernameController.text}',
+          'pass': '${passwordController.text}',
+        }),
       );
+
+      Map<String, dynamic> parsedResponse = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        token.value = '${parsedResponse['token']}';
+        Get.to(() => SuccessPage());
+      } else {
+        showSnackbar(context: context, message: '${parsedResponse['error']}');
+      }
+    } catch (e) {
+      print(e);
+      showSnackbar(context: context, message: 'Unable to connect to server');
     }
   }
+}
+
+showSnackbar({required BuildContext context, required String message}) {
+  ScaffoldMessenger.of(context).clearSnackBars();
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: Colors.black45,
+      behavior: SnackBarBehavior.floating,
+      content: Text(
+        message,
+        style: TextStyle(
+          color: Colors.red,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      margin: EdgeInsets.all(20),
+    ),
+  );
 }
